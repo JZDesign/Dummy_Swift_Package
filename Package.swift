585828959fe6991ef2cdcc8ffddf9f79aab2ebd8
Jacob Rakidzich <jacob.rakidzich@daveramsey.com>
2020-08-09 13:13:00 -0500

    remove platforms block

--- Package.swift
+++ Package.swift
@@ -5,12 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "Dummy_Swift_Package",
-    platforms: [
-        .iOS(.v13),
-        .tvOS(.v11),
-        .macOS(.v10_14),
-        .watchOS(.v5)
-    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
