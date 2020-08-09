14aa38493b9d39d28a218bb87c34b297676d6c5f
Jacob Rakidzich <jacob.rakidzich@daveramsey.com>
2020-08-09 05:37:25 -0500

    package changes

--- Package.swift
+++ Package.swift
@@ -4,19 +4,27 @@
 import PackageDescription
 
 let package = Package(
-    name: "Dummy_Swift_Package",
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+    name: "DummySwiftPackage",
+    platforms: [
+        .iOS(.v13),
+        .tvOS(.v11),
+        .macOS(.v10_14),
+        .watchOS(.v5)
     ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "DummySwiftPackage",
+            type: .dynamic,
+            targets: ["DummySwiftPackage"]),
+    ], dependencies: [
+    ],
+       targets: [
         .target(
-            name: "Dummy_Swift_Package",
+            name: "DummySwiftPackage",
             dependencies: []),
         .testTarget(
-            name: "Dummy_Swift_PackageTests",
-            dependencies: ["Dummy_Swift_Package"]),
+            name: "DummySwiftPackageTests",
+            dependencies: ["DummySwiftPackage"]),
     ]
 )
